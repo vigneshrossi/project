eef50af8fbf9da4a3a0a199964ab9a67ec13b994
vigneshrossi <vigneshamu93@gmailcom>
2020-02-21 17:48:17 +0530

    add name

--- src/test/java/task/snapDeal.java
+++ src/test/java/task/snapDeal.java
@@ -24,7 +24,7 @@ public class snapDeal {
 			System.out.println(text1);
 			System.out.println("///////////////////////////////////");
 			System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-			
+			System.out.println("bala");
 		}
 	}
 
