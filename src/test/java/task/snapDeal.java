5909525cda12d9cceef4865c28ed8bd8306e8c4b
vigneshrossi <vigneshamu93@gmailcom>
2020-02-23 19:19:57 +0530

    ece

--- src/test/java/task/snapDeal.java
+++ src/test/java/task/snapDeal.java
@@ -1,9 +1,6 @@
 package task;
 public class snapDeal {
 	public static void main(String[] args) {
-	System.out.println("11111111111111111111111111111");
-	System.out.println("99999999999999999999999999999");
-	System.out.println("88888888888888888888888888888");
-	System.out.println("77777777777777777777777777777");
+	System.out.println("Electrical");
 	}
 }
